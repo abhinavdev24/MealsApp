7714d8d125de3c90d3bea775334d0437b81ab461
Abhinav Kumar <1rn18cs002.abhinav@gmail.com>
2024-05-07 19:55:14 +0530

    feat: display item in grid

---
+++ README.md
@@ -0,0 +1,9 @@
+# Meals App
+
+### Description:
+
+The Meals App is a mobile application built using React Native.
+
+### Learning Experience:
+
+Building this app provided a hands-on learning experience with various React Native concepts like multi screen navigation.
---
+++ components/CategoryGridTile.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Pressable, View, Text, StyleSheet } from "react-native";
+
+const CategoryGridTile = ({ title, color }) => {
+  return (
+    <View style={[styles.gridItem, { backgroundColor: color }]}>
+      <Pressable
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+        android_ripple={{ color: "#ccc" }}
+      >
+        <View style={styles.innerContainer}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    backgroundColor: "white",
+    overflow: "hidden",
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
+
+export default CategoryGridTile;
